9bad4124311e5295591d9771289df78fdafb32b0
FeelYon <tokpli33@mail.ru>
2025-07-04 21:38:33 +0400

    replace h & ch

--- inc/system_definitions.h
+++ inc/system_definitions.h
@@ -58,8 +58,8 @@ SystemState get_sys_state();
 #define USB_UART_HANDLE huart1
 
 //______________________________BUZZER
-#define BUZZER_TIM_HANDLE TIM_CHANNEL_3
-#define BUZZER_TIM_DEF htim4
+#define BUZZER_TIM_HANDLE htim4
+#define BUZZER_TIM_CHANNEL TIM_CHANNEL_3
 
 //_____________________________LED
 #define LED1_PORT GPIOB
--- src/sensors/buzzer.c
+++ src/sensors/buzzer.c
@@ -3,18 +3,18 @@
 
 void buzzer_set_freq(uint32_t freq_hz) {
     uint32_t clock = HAL_RCC_GetPCLK1Freq();
-    uint32_t prescaler = BUZZER_TIM_DEF.Instance->PSC;
+    uint32_t prescaler = BUZZER_TIM_HANDLE.Instance->PSC;
     uint32_t period = (clock / ((prescaler + 1) * freq_hz)) - 1;
 
-    __HAL_TIM_SET_PRESCALER(&BUZZER_TIM_DEF, prescaler);
-    __HAL_TIM_SET_AUTORELOAD(&BUZZER_TIM_DEF, period);
-    __HAL_TIM_SET_COMPARE(&BUZZER_TIM_DEF, BUZZER_TIM_HANDLE, period / 2);
+    __HAL_TIM_SET_PRESCALER(&BUZZER_TIM_HANDLE, prescaler);
+    __HAL_TIM_SET_AUTORELOAD(&BUZZER_TIM_HANDLE, period);
+    __HAL_TIM_SET_COMPARE(&BUZZER_TIM_HANDLE, BUZZER_TIM_CHANNEL, period / 2);
 }
 
 void buzzer_start() {
-    HAL_TIM_PWM_Start(&BUZZER_TIM_DEF, BUZZER_TIM_HANDLE);
+    HAL_TIM_PWM_Start(&BUZZER_TIM_HANDLE, BUZZER_TIM_CHANNEL);
 }
 
 void buzzer_stop() {
-    HAL_TIM_PWM_Stop(&BUZZER_TIM_DEF, BUZZER_TIM_HANDLE);
+    HAL_TIM_PWM_Stop(&BUZZER_TIM_HANDLE, BUZZER_TIM_CHANNEL);
 }
